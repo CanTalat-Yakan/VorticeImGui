d57582e1f14e9ecd6a7ac0b35fed14e77781758d
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2024-02-24 12:17:04 +0100

    ­

--- VorticeImGuiDx12/Assets/Resources/Shaders/ImGui.hlsl
+++ VorticeImGuiDx12/Assets/Resources/Shaders/ImGui.hlsl
@@ -3,18 +3,21 @@
 sampler sampler0;
 Texture2D texture0;
 
-PS_INPUT VS(VS_INPUT input)
+PS_INPUT_UI VS(VS_INPUT_UI input)
 {
-    PS_INPUT output;
+    PS_INPUT_UI output;
+
     output.pos = mul(ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
     output.col = input.col;
     output.uv = input.uv;
+
     return output;
 }
 
-float4 PS(PS_INPUT input) : SV_Target
+float4 PS(PS_INPUT_UI input) : SV_Target
 {
     float4 output = input.col * texture0.Sample(sampler0, input.uv);
     clip(output.a - 0.1);
+
     return output;
 }
--- VorticeImGuiDx12/Assets/Resources/Shaders/Include/Common.hlsli
+++ VorticeImGuiDx12/Assets/Resources/Shaders/Include/Common.hlsli
@@ -1,18 +1,18 @@
-struct PS_INPUT
+struct PS_INPUT_UI
 {
     float4 pos : SV_POSITION;
     float4 col : COLOR0;
     float2 uv : TEXCOORD0;
 };
 
-struct VS_INPUT
+struct VS_INPUT_UI
 {
     float2 pos : POSITION;
     float4 col : COLOR0;
     float2 uv : TEXCOORD0;
 };
 
-cbuffer vertexBuffer : register(b0)
+cbuffer VertexBuffer : register(b0)
 {
     float4x4 ProjectionMatrix;
 };
