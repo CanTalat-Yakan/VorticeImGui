5061fac9d8cc42779e7f15a87cb7ffb6259eb0b4
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2024-02-25 17:44:03 +0100

    ­

--- VorticeImGuiDx12/Assets/Rendering/Data/Config.cs
+++ VorticeImGuiDx12/Assets/Rendering/Data/Config.cs
@@ -1,54 +1,5 @@
 ﻿namespace Engine.Data;
 
-public sealed class Config
-{
-    public WindowData WindowData;
-
-    public CameraProjection CameraProjection = CameraProjection.Perspective;
-    public RenderMode RenderMode = RenderMode.Shaded;
-
-    public PresentInterval VSync = PresentInterval.Immediate;
-
-    public double ResolutionScale = 1;
-
-    public MultiSample MultiSample = MultiSample.None;
-    public int SupportedSampleCount = 1;
-    public int QualityLevels = 0;
-
-    public bool Debug = true;
-    public bool GUI = true;
-
-    public static Config GetDefault(
-        PresentInterval presentInterval = PresentInterval.Immediate,
-        MultiSample multiSample = MultiSample.x2,
-        double resolutionScale = 1,
-        string title = "3D Engine",
-        int width = 1080,
-        int height = 720)
-    {
-        Config config = new();
-        config.SetVSync(presentInterval);
-        config.SetMSAA(multiSample);
-        config.SetResolutionScale(resolutionScale);
-        config.SetWindowData(title, width, height);
-
-        return config;
-    }
-
-    public void SetWindowData(string title, int width, int height) =>
-        WindowData = new(title, width, height);
-
-    public void SetMSAA(MultiSample multiSample) =>
-        MultiSample = multiSample;
-
-    public void SetVSync(PresentInterval interval) =>
-        VSync = interval;
-
-    public void SetResolutionScale(double scale) =>
-        ResolutionScale = scale;
-
-}
-
 public struct WindowData(string title, int width, int height)
 {
     public string Title = title;
@@ -85,4 +36,52 @@ public enum MultiSample
     x2 = 2,
     x4 = 4,
     x8 = 8,
+}
+
+public sealed class Config
+{
+    public WindowData WindowData;
+
+    public CameraProjection CameraProjection = CameraProjection.Perspective;
+    public RenderMode RenderMode = RenderMode.Shaded;
+
+    public PresentInterval VSync = PresentInterval.Immediate;
+
+    public double ResolutionScale = 1;
+
+    public MultiSample MultiSample = MultiSample.None;
+    public int SupportedSampleCount = 1;
+    public int QualityLevels = 0;
+
+    public bool Debug = true;
+    public bool GUI = true;
+
+    public static Config GetDefault(
+        PresentInterval presentInterval = PresentInterval.Immediate,
+        MultiSample multiSample = MultiSample.x2,
+        double resolutionScale = 1,
+        string title = "ImGui DirectX12",
+        int width = 1080,
+        int height = 720)
+    {
+        Config config = new();
+        config.SetVSync(presentInterval);
+        config.SetMSAA(multiSample);
+        config.SetResolutionScale(resolutionScale);
+        config.SetWindowData(title, width, height);
+
+        return config;
+    }
+
+    public void SetWindowData(string title, int width, int height) =>
+        WindowData = new(title, width, height);
+
+    public void SetMSAA(MultiSample multiSample) =>
+        MultiSample = multiSample;
+
+    public void SetVSync(PresentInterval interval) =>
+        VSync = interval;
+
+    public void SetResolutionScale(double scale) =>
+        ResolutionScale = scale;
 }
